86003a96aa605aaa3667a341f4f4a8ccae67bff3
root <root@Kel.com>
2015-07-28 19:14:52 +0800

    Update packages.

--- elpa/magit-20150727.1535/magit-remote.el
+++ elpa/magit-20150727.1535/magit-remote.el
@@ -38,12 +38,17 @@
   "Clone the REPOSITORY to DIRECTORY."
   (interactive
    (let  ((url (magit-read-string "Clone repository")))
-     (list url (read-directory-name
-                "Clone to: " nil nil nil
-                (and (string-match "\\([^./]+\\)\\(\\.git\\)?$" url)
-                     (match-string 1 url))))))
+     (list url (file-name-as-directory
+                (expand-file-name
+                 (read-directory-name
+                  "Clone to: " nil nil nil
+                  (and (string-match "\\([^./]+\\)\\(\\.git\\)?$" url)
+                       (match-string 1 url))))))))
   (make-directory directory t)
-  (magit-run-git "clone" repository directory))
+  (message "Cloning %s..." repository)
+  (magit-call-git "clone" repository directory)
+  (message "Cloning %s...done" repository)
+  (magit-status-internal directory))
 
 ;;; Setup
 
@@ -169,7 +174,7 @@ then read the remote."
               (?d "Dry run"       "--dry-run")
               (?u "Set upstream"  "--set-upstream"))
   :actions  '((?P "Current"    magit-push-current)
-              (?q "Quickly"    magit-push-quickly)
+              (?Q "Quickly"    magit-push-quickly)
               (?t "Tags"       magit-push-tags)
               (?o "Other"      magit-push)
               (?i "Implicitly" magit-push-implicitly)

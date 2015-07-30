5586a9cfb0fb434660190616f95b2d6df879675d
ChrisZheng <zklhp@sina.com>
2015-07-30 03:21:02 +0200

    Update magit.

--- elpa/magit-20150729.518/magit-pkg.el
+++ elpa/magit-20150729.518/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20150728.1305" "A Git porcelain inside Emacs"
+(define-package "magit" "20150729.518" "A Git porcelain inside Emacs"
   '((emacs "24.4")
     (dash "2.10.0")
     (with-editor "2.1.0")

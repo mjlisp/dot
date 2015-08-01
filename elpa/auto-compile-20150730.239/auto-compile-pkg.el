9e56081c2e77280f2071c7daa394b53824aa8d30
ChrisZheng <zklhp@sina.com>
2015-08-01 05:23:23 +0200

    Update packages.

--- elpa/auto-compile-20150730.239/auto-compile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "auto-compile" "20150730.239" "automatically compile Emacs Lisp libraries" '((emacs "24.3") (cl-lib "0.5") (dash "2.1") (packed "0.3.5")) :url "https://github.com/tarsius/auto-compile" :keywords '("compile" "convenience" "lisp"))
--- elpa/auto-compile-20150731.545/auto-compile-autoloads.el
+++ elpa/auto-compile-20150731.545/auto-compile-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "auto-compile" "auto-compile.el" (21947 969
+;;;### (autoloads nil "auto-compile" "auto-compile.el" (21948 14926
 ;;;;;;  0 0))
 ;;; Generated autoloads from auto-compile.el
 

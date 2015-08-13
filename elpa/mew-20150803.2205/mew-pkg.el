5c042f94f656bf0d257f36f8e578748280af4b15
ChrisZheng <zklhp@sina.com>
2015-08-13 02:39:42 +0200

    Update.

--- elpa/mew-20150803.2205/mew-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "mew" "20150803.2205" "Messaging in the Emacs World" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/mew-20150812.607/mew-pkg.el
@@ -0,0 +1,4 @@
+(define-package "mew" "20150812.607" "Messaging in the Emacs World" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

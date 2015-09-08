d88195d55159659132a43eb72ee7a8a0e6b67b4e
ChrisZheng <zklhp@sina.com>
2015-09-08 08:28:43 +0200

    Try to push the updated packages.

---
+++ elpa/helm-core-20150907.1418/helm-core-pkg.el
@@ -0,0 +1,8 @@
+(define-package "helm-core" "20150907.1418" "Development files for Helm"
+  '((emacs "24")
+    (cl-lib "0.5")
+    (async "1.4"))
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

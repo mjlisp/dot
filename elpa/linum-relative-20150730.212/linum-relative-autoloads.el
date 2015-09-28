ad42bfc648937203487848419c0edebf4fde117b
ChrisZheng <zklhp@sina.com>
2015-09-28 20:20:15 +0200

    Update.

--- elpa/linum-relative-20150730.212/linum-relative-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; linum-relative-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("linum-relative.el") (21947 968 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; linum-relative-autoloads.el ends here

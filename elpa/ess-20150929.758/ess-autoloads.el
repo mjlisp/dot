8a1ef19f56489032d4ff90adef93ab193b2b1644
ChrisZheng <zklhp@sina.com>
2015-09-30 02:42:00 +0200

    Update.

--- elpa/ess-20150929.758/ess-autoloads.el
+++ elpa/ess-20150929.758/ess-autoloads.el
@@ -57,7 +57,7 @@ to R, put them in the variable `inferior-R-args'." t)
 (autoload 'S "ess-site" "Call 'S'." t)
 
 
-;;;### (autoloads nil nil ("ess-pkg.el") (22020 59043 0 0))
+;;;### (autoloads nil nil ("ess-pkg.el") (22027 10365 0 0))
 
 ;;;***
  ; Local variables section
--- elpa/ess-20150929.758/ess-pkg.el
+++ elpa/ess-20150929.758/ess-pkg.el
@@ -1,4 +1,4 @@
-(define-package "ess" "20150924.1356" "Emacs Speaks Statistics"
+(define-package "ess" "20150929.758" "Emacs Speaks Statistics"
   '((julia-mode "0.3"))
   :url "http://ess.r-project.org")
 ;; Local Variables:

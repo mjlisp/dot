00e31b1dcf36d5ad89561ecb7170f109c2a174f7
ChrisZheng <zklhp@sina.com>
2015-10-06 16:09:50 +0200

    Update.

--- elpa/dash-20151004.257/dash-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dash" "20151004.257" "A modern list library for Emacs" 'nil :keywords '("lists"))
---
+++ elpa/dash-20151005.2032/dash-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dash" "20151005.2032" "A modern list library for Emacs" 'nil :keywords '("lists"))

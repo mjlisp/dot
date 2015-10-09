0743afa8de496d308bc62e3e5d9dddde3223a4c6
ChrisZheng <zklhp@sina.com>
2015-10-09 05:55:13 +0200

    Update.

---
+++ elpa/uimage-20151006.259/uimage-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "uimage" "20151006.259" "Url image minor mode." 'nil :keywords '("lisp" "url" "image"))
--- init.el
+++ init.el
@@ -26,7 +26,7 @@
  '(display-time-24hr-format t)
  '(display-time-mode t)
  '(electric-pair-mode t)
- '(ess-directory-containing-R "E:" t)
+ '(ess-directory-containing-R "E:")
  '(ess-eval-visibly (quote nowait))
  '(font-lock-maximum-decoration t)
  '(gdb-many-windows t)
@@ -91,7 +91,7 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (helm ess auto-compile rainbow-delimiters w32-browser srefactor yaoddmuse yaml-mode vlf smart-compile readline-complete rainbow-mode nlinum loc-changes linum-relative js2-refactor js-comint htmlize go-mode gnuplot-mode fic-mode fasm-mode dot-mode disaster dired-k cygwin-mount csv-mode bash-completion auto-complete-auctex auctex alpha ac-octave ac-math ac-js2 ac-etags ac-dabbrev ac-c-headers)))
+    (async auto-complete auto-complete-pcmp dash dired+ git-commit helm-core hydra js2-mode julia-mode log4e magit magit-popup markdown-mode math-symbol-lists mew multiple-cursors org packed pandoc-mode php-mode popup powerline s simple-httpd skewer-mode synonyms with-editor yasnippet yaxception uimage helm ess auto-compile rainbow-delimiters w32-browser srefactor yaoddmuse yaml-mode vlf smart-compile readline-complete rainbow-mode nlinum loc-changes linum-relative js2-refactor js-comint htmlize go-mode gnuplot-mode fic-mode fasm-mode dot-mode disaster dired-k cygwin-mount csv-mode bash-completion auto-complete-auctex auctex alpha ac-octave ac-math ac-js2 ac-etags ac-dabbrev ac-c-headers)))
  '(preview-TeX-style-dir "" t)
  '(preview-auto-cache-preamble t)
  '(preview-default-document-pt 25.0)

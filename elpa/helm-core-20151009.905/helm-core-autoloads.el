60a908edbc4b09f1f7f05a3b1c7ff05ec1e4c2c4
ChrisZheng <zklhp@sina.com>
2015-10-10 06:18:04 +0200

    Update.

--- elpa/helm-core-20151009.905/helm-core-autoloads.el
+++ elpa/helm-core-20151009.905/helm-core-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "helm" "helm.el" (22039 14169 0 0))
+;;;### (autoloads nil "helm" "helm.el" (22040 23551 0 0))
 ;;; Generated autoloads from helm.el
 
 (autoload 'helm-define-multi-key "helm" "\
@@ -190,7 +190,7 @@ Enable/disable helm debug from outside of helm session.
 ;;;***
 
 ;;;### (autoloads nil nil ("helm-core-pkg.el" "helm-lib.el" "helm-multi-match.el"
-;;;;;;  "helm-source.el") (22039 14169 0 0))
+;;;;;;  "helm-source.el") (22040 23551 0 0))
 
 ;;;***
 

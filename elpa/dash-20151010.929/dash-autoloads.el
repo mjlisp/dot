a97d1a5e3234bb8798bf11e693945d51d5740faf
ChrisZheng <zklhp@sina.com>
2015-10-11 03:14:09 +0200

    Update.

--- elpa/dash-20151010.929/dash-autoloads.el
+++ elpa/dash-20151010.929/dash-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("dash.el") (22038 4558 0 0))
+;;;### (autoloads nil nil ("dash.el") (22041 44061 0 0))
 
 ;;;***
 
--- elpa/with-editor-20151010.1026/with-editor-autoloads.el
+++ elpa/with-editor-20151010.1026/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22029 10670 0 0))
+;;;;;;  (22041 44047 0 0))
 
 ;;;***
 

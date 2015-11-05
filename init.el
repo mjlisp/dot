b0f6c4ee0f502102fb8e09577e6690d3f701022d
ChrisZheng <zklhp@sina.com>
2015-11-05 02:35:05 +0100

    Update.

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
  '(TeX-PDF-mode t)
  '(TeX-engine (quote xetex))
  '(TeX-queue-command nil)
- ;; '(TeX-shell "bash" t)
  '(ac-auto-show-menu 0.1)
  '(c-echo-syntactic-information-p t)
  '(column-number-mode t)
@@ -94,7 +93,7 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (git-commit hydra linum-relative visual-fill-column writegood-mode langtool ess async auto-complete auto-complete-pcmp dash dired+ js2-mode julia-mode log4e magit magit-popup markdown-mode math-symbol-lists mew multiple-cursors org packed pandoc-mode php-mode popup powerline s simple-httpd skewer-mode synonyms with-editor yasnippet yaxception uimage auto-compile rainbow-delimiters w32-browser srefactor yaoddmuse yaml-mode vlf smart-compile readline-complete rainbow-mode nlinum loc-changes js2-refactor js-comint htmlize go-mode gnuplot-mode fic-mode fasm-mode dot-mode disaster dired-k cygwin-mount csv-mode bash-completion auto-complete-auctex auctex alpha ac-octave ac-math ac-js2 ac-etags ac-dabbrev ac-c-headers)))
+    (git-commit hydra linum-relative visual-fill-column writegood-mode langtool ess async auto-complete auto-complete-pcmp dash dired+ js2-mode julia-mode log4e magit-popup markdown-mode math-symbol-lists mew multiple-cursors org packed pandoc-mode php-mode popup powerline s simple-httpd skewer-mode synonyms with-editor yasnippet yaxception uimage auto-compile rainbow-delimiters w32-browser srefactor yaoddmuse yaml-mode vlf smart-compile readline-complete rainbow-mode nlinum loc-changes js2-refactor js-comint htmlize go-mode gnuplot-mode fic-mode fasm-mode dot-mode disaster dired-k cygwin-mount csv-mode bash-completion auto-complete-auctex auctex alpha ac-octave ac-math ac-js2 ac-etags ac-dabbrev ac-c-headers)))
  '(preview-TeX-style-dir "" t)
  '(preview-auto-cache-preamble t)
  '(preview-default-document-pt 25.0)

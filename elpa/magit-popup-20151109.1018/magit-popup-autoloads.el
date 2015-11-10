2580b300dc2cd81ee0a05c9530c01f3703e10b4c
ChrisZheng <zklhp@sina.com>
2015-11-10 04:47:41 +0100

    Update.

--- elpa/magit-popup-20151109.1018/magit-popup-autoloads.el
+++ elpa/magit-popup-20151109.1018/magit-popup-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
-;;;;;;  (22074 42083 0 0))
+;;;;;;  (22081 26755 0 0))
 
 ;;;***
 

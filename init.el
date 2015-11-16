663d73995e45b6767a019acd8f2f5e4381f4e25d
ChrisZheng <zklhp@sina.com>
2015-11-16 13:03:41 +0100

    Update.

--- init.el
+++ init.el
@@ -209,3 +209,9 @@
 ;; (load "~/.emacs.d/paradox-token.el")
 
 (put 'upcase-region 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

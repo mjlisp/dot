1a402fbc166762a13b9702459e8d4421186d5f36
ChrisZheng <zklhp@sina.com>
2015-11-19 12:07:39 +0800

    Update.

--- elpa/magit-20151118.412/magit-pkg.el
+++ elpa/magit-20151118.412/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20151114.1619" "A Git porcelain inside Emacs"
+(define-package "magit" "20151118.412" "A Git porcelain inside Emacs"
   '((emacs "24.4")
     (async "20150909.2257")
     (dash "20151021.113")

e52c3496a2adc2887eeb7f5d2a27eb38bd3c363d
ChrisZheng <zklhp@sina.com>
2015-12-10 09:00:47 +0800

    Update.

--- elpa/with-editor-20151209.732/with-editor-autoloads.el
+++ elpa/with-editor-20151209.732/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22103 53496 0 0))
+;;;;;;  (22120 52897 0 0))
 
 ;;;***
 

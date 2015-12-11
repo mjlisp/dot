f8c12cf46339cef0135914bcd5dde2771ed3955c
ChrisZheng <zklhp@sina.com>
2015-12-11 19:51:04 +0800

    Update.

--- elpa/auto-complete-20151211.227/auto-complete-autoloads.el
+++ elpa/auto-complete-20151211.227/auto-complete-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "auto-complete" "auto-complete.el" (22086 38585
+;;;### (autoloads nil "auto-complete" "auto-complete.el" (22122 47230
 ;;;;;;  0 0))
 ;;; Generated autoloads from auto-complete.el
 
@@ -41,7 +41,7 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 ;;;***
 
 ;;;### (autoloads nil "auto-complete-config" "auto-complete-config.el"
-;;;;;;  (22086 38586 0 0))
+;;;;;;  (22122 47230 0 0))
 ;;; Generated autoloads from auto-complete-config.el
 
 (autoload 'ac-config-default "auto-complete-config" "\
@@ -51,7 +51,7 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 
 ;;;***
 
-;;;### (autoloads nil nil ("auto-complete-pkg.el") (22086 38585 0
+;;;### (autoloads nil nil ("auto-complete-pkg.el") (22122 47230 0
 ;;;;;;  0))
 
 ;;;***

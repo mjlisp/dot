00db82b389b73ce20850e7dd7c88e691d544adab
ChrisZheng <zklhp@sina.com>
2015-12-15 17:55:43 +0800

    Update.

--- elpa/markdown-mode-20151214.2257/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20151214.2257/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22116 58536
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22127 58255
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 

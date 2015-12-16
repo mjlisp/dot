1abd55df2960072e62e9f8d2b2641ce8aa369df9
ChrisZheng <zklhp@sina.com>
2015-12-16 17:06:32 +0800

    Update.

--- elpa/dired+-20151215.654/dired+-autoloads.el
+++ elpa/dired+-20151215.654/dired+-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "dired+" "dired+.el" (22082 54628 0 0))
+;;;### (autoloads nil "dired+" "dired+.el" (22129 10571 0 0))
 ;;; Generated autoloads from dired+.el
 
 (defvar diff-switches "-c" "\
---
+++ elpa/dired+-20151215.654/dired+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired+" "20151215.654" "Extensions to Dired." 'nil :url "http://www.emacswiki.org/dired+.el" :keywords '("unix" "mouse" "directories" "diredp" "dired"))

f3c2d6a23412dd9338a7fb61a15e01233b8b629d
ChrisZheng <zklhp@sina.com>
2015-12-17 11:50:04 +0800

    Update.

--- elpa/dash-20151216.1315/dash-autoloads.el
+++ elpa/dash-20151216.1315/dash-autoloads.el
@@ -1,10 +1,9 @@
 ;;; dash-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (22057 37955
-;;;;;;  0 0))
+;;;### (autoloads nil nil ("dash.el") (22130 12483 0 0))
 
 ;;;***
 

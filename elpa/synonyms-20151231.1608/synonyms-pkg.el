7e1414b66f8fdd8647d03eb50bece441d871f0e0
ChrisZheng <zklhp@sina.com>
2016-01-01 20:44:40 +0800

    Update.

---
+++ elpa/synonyms-20151231.1608/synonyms-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "synonyms" "20151231.1608" "Look up synonyms for a word or phrase in a thesaurus." 'nil :url "http://www.emacswiki.org/synonyms.el" :keywords '("text" "dictionary" "thesaurus" "spelling" "apropos" "help"))

003510fa796b28f43c05c263563a99ea5b28c2f8
ChrisZheng <zklhp@sina.com>
2016-01-02 09:13:10 +0800

    Update.

--- elpa/w32-browser-20151231.1620/w32-browser-autoloads.el
+++ elpa/w32-browser-20151231.1620/w32-browser-autoloads.el
@@ -1,10 +1,9 @@
 ;;; w32-browser-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("w32-browser.el") (21853 64521 459536
-;;;;;;  0))
+;;;### (autoloads nil nil ("w32-browser.el") (22151 8708 0 0))
 
 ;;;***
 
--- elpa/yaml-mode-20151218.354/yaml-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yaml-mode" "20151218.354" "Major mode for editing YAML files" '((emacs "24.1")) :keywords '("data" "yaml"))

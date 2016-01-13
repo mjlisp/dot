1de9047b8f20e68069d793235960ca10bccf4d52
ChrisZheng <zklhp@sina.com>
2016-01-13 09:12:11 +0800

    Revert last commit.

--- .init/common-win.el
+++ .init/common-win.el
@@ -68,6 +68,6 @@
 (autoload 'dired-async-mode "dired-async.el" nil t)
 (dired-async-mode 1)
 
-settings(add-to-list 'tramp-connection-properties
-                  (list (regexp-quote ".88.")
-                        "busybox" t))
+;; (add-to-list 'tramp-connection-properties
+;;                   (list (regexp-quote ".88.")
+;;                         "busybox" t))

0b7203109665302073a128802ad1afd446c1f13a
ChrisZheng <zklhp@sina.com>
2016-02-13 12:28:05 +0800

    Change default font.

--- .init/font-settings.el
+++ .init/font-settings.el
@@ -1,8 +1,12 @@
 ;; Font Settings
 
 ;; Setting English Font
-(set-face-attribute
- 'default nil :family "Inconsolata" :height (if (string-equal system-type "windows-nt") 160 120))
+;; (set-face-attribute
+;;  'default nil :family "Inconsolata" :height )
+(add-to-list 'default-frame-alist
+             (if (string-equal system-type "windows-nt")
+		 '(font . "Inconsolata-18")
+	       '(font . "Inconsolata-14")))
 
 ;; (set-fontset-font t 'unicode-bmp "STIX" nil 'append)
 ;; (set-fontset-font t 'symbol "Segoe UI Emoji" nil 'append)
@@ -13,7 +17,7 @@
   (set-fontset-font t ;; (frame-parameter nil 'font)
   		    charset
 		    (if (string-equal system-type "windows-nt")
-		    	(font-spec :family "冬青黑体简体中文 W3")
+		        (font-spec :family "冬青黑体简体中文 W3")
 		      (font-spec :name "Hiragino Sans GB"))))
 			;; 微软雅黑
 			;; 冬青黑体简体中文 W3
@@ -23,9 +27,9 @@
 
 (when (string-equal system-type "windows-nt")
   (set-fontset-font t 'unicode "Segoe UI Symbol" nil 'append)
-  (set-fontset-font t '(#x1F600 . #x1F64F) "Segoe UI Symbol")) ; Emoji
-
-(set-fontset-font t '(#xE000 . #xF8FF) "STIX") ; Private Use Areas
+  (set-fontset-font t '(#x1F600 . #x1F64F) "Segoe UI Symbol") ; Emoji
+  (set-fontset-font t '(#xE000 . #xF8FF) "STIX") ; Private Use Areas
+  )
 
 (set-fontset-font t ?– "Symbola")
 (set-fontset-font t ?′ "Symbola")
@@ -33,29 +37,6 @@
 
 (set-fontset-font "fontset-default" nil 
                   (font-spec :name "Symbola"))
-
-;; (set-fontset-font t ;; (frame-parameter nil 'font)
-;; 		    'symbol
-;; 		    (font-spec :family "Symbola"))
-
-
-;; (set-fontset-font t 'unicode "Symbola" nil 'prepend)
-
-;; (set-fontset-font t
-;; 		    '(#x0300 . #x0306)
-;; 		    (font-spec :family "STIX"))
-
-;; (set-fontset-font (frame-parameter nil 'font)
-;; 		  'ISO10646
-;; 		  (font-spec :family "Symbola"))
-
-;; (set-fontset-font (frame-parameter nil 'font)
-;; 		    '(#x1d4b6 . #x1d537)
-;; 		    (font-spec :family "Symbola"))
-
-;; (set-fontset-font (frame-parameter nil 'font)
-;; 		    '(#xe833 . #xe835)
-;; 		    (font-spec :family "宋体-方正超大字符集"))
 
 (defface strike-through
   '((t :strike-through t))

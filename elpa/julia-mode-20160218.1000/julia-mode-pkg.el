3ca4b57ce163f000899471f30d1498027c2ca0c2
ChrisZheng <zklhp@sina.com>
2016-02-20 12:03:31 +0800

    Update.

--- elpa/julia-mode-20160218.1000/julia-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "julia-mode" "20160218.1000" "Major mode for editing Julia source code" 'nil :url "https://github.com/JuliaLang/julia" :keywords '("languages"))

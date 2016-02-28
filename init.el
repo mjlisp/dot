37093d556d2c2b12150f630d5260e51cdc9f8886
ChrisZheng <zklhp@sina.com>
2016-02-28 20:57:11 +0800

    Update.

--- init.el
+++ init.el
@@ -207,3 +207,9 @@
     (transparency-set-value 85))
   ;; 字体设置
   (load "~/.emacs.d/.init/font-settings"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

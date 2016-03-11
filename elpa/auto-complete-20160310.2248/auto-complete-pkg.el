f7d37262eeb5b08f6bf9dd9aab230897dc1ef1ab
Chris Zheng <zklhp@sina.com>
2016-03-11 18:45:11 +0800

    Update.

--- elpa/auto-complete-20160310.2248/auto-complete-pkg.el
+++ elpa/auto-complete-20160310.2248/auto-complete-pkg.el
@@ -1,4 +1,4 @@
-(define-package "auto-complete" "20160229.1706" "Auto Completion for GNU Emacs"
+(define-package "auto-complete" "20160310.2248" "Auto Completion for GNU Emacs"
   '((popup "0.5.0")
     (cl-lib "0.5")))
 ;; Local Variables:

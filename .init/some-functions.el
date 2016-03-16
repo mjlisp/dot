23e01b6c7edb437d762099219789145752db5a67
Chris Zheng <zklhp@sina.com>
2016-03-16 12:34:55 +0800

    Use prefix to disable keyrate setting.

--- .init/some-functions.el
+++ .init/some-functions.el
@@ -133,4 +133,6 @@ output in temp buffer `*BC Output*'.  With prefix, insert the output."
 (defun my-keyrate ()
   (interactive)
   (when (string-equal system-type "windows-nt")
-    (call-process-shell-command "/f/diy/keyrate 200 12" nil 0)))
+    (if current-prefix-arg
+	(call-process-shell-command "/f/diy/keyrate" nil 0)
+      (call-process-shell-command "/f/diy/keyrate 200 12" nil 0))))

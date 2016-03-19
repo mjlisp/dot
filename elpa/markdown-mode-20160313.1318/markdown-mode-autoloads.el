6a255c1a3c62cfec2432be4d5bb0c2d6ac5063fb
Chris Zheng <zklhp@sina.com>
2016-03-19 19:59:45 +0800

    Update.

--- elpa/markdown-mode-20160313.1318/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20160313.1318/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22246 9752
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22253 15362
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
@@ -13,8 +13,6 @@ Major mode for editing Markdown files.
 \(fn)" t nil)
 
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 

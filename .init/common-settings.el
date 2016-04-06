6f614bafe128e6264c89d6a0c56acd217f433784
Chris Zheng <zklhp@sina.com>
2016-04-06 20:27:37 +0800

    Update.

--- .init/common-settings.el
+++ .init/common-settings.el
@@ -69,7 +69,8 @@
 (when (fboundp 'rainbow-delimiters-mode)
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
-(when (featurep 'nyan-mode)
+(when (or (require 'nyan-mode)
+	  (featurep 'nyan-mode))
   (nyan-mode 1))
 
 (when (and (require 'auto-compile)

7f671f56466ddc9e88177895dddf50ac9f36c5ba
Chris Zheng <zklhp@sina.com>
2016-04-15 13:33:07 +0800

    Update.

--- elpa/ess-20160414.1823/ess-pkg.el
+++ elpa/ess-20160414.1823/ess-pkg.el
@@ -1,4 +1,4 @@
-(define-package "ess" "20160413.1157" "Emacs Speaks Statistics"
+(define-package "ess" "20160414.1823" "Emacs Speaks Statistics"
   '((julia-mode "0.3"))
   :url "http://ess.r-project.org")
 ;; Local Variables:
